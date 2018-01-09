c3c8da39c2ded8766fc32e2d230cb484069221a7
Miguel Oviedo <migueloviedoc@gmail.com>
2018-01-08 22:46:42 -0800

    ADD EditProfilePage

--- StyleUs/ViewModel/EditProfilePageViewModel.cs
+++ StyleUs/ViewModel/EditProfilePageViewModel.cs
@@ -1,10 +1,40 @@
-﻿using System;
-namespace StyleUs.ViewModel
-{
-    public class EditProfilePageViewModel
+﻿using StyleUs.ViewModel;
+using Xamarin.Forms;
+using Prism.Navigation;
+using Prism.Commands;
+using Prism.Events;
+using System.ComponentModel;
+
+public class EditProfilePageViewModel
     {
-        public EditProfilePageViewModel()
+        public class EditProfileViewModel : ContentPage
+    {
+
+        public DelegateCommand back { get; set; }
+
+        readonly INavigationService navigation;
+        IEventAggregator events;
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        public EditProfileViewModel(INavigationService navigationService, IEventAggregator eventAgregator)
         {
+            navigation = navigationService;
+            events = eventAgregator;
+
+            back = new DelegateCommand(OnBackClick);
         }
+
+
+        public void OnBackClick()
+        {
+            //navigation.NavigateAsync("AboutUsPage");
+            navigation.GoBackAsync();
+            //navigation.NavigateAsync(new Uri("/MainTabbedPage/MenuPage", UriKind.Absolute));
+
+        }
+
+
+    
     }
 }
