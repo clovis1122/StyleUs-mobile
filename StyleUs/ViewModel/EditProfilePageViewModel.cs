f6338e928c38d1364cdd397dc40d34541e933f5a
Adeury09 <adeury09@github.com>
2018-01-10 18:48:41 -0400

    Add cancel Button and Save Button on the Edit profile View

--- StyleUs/ViewModel/EditProfilePageViewModel.cs
+++ StyleUs/ViewModel/EditProfilePageViewModel.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using StyleUs.ViewModel;
+using Xamarin.Forms;
+using Prism.Navigation;
+using Prism.Commands;
+using Prism.Events;
+using System.ComponentModel;
+
 namespace StyleUs.ViewModel
 {
-    public class EditProfilePageViewModel
+    public class EditProfilePageViewModel : ContentPage
     {
-        public EditProfilePageViewModel()
+
+        public DelegateCommand cancel { get; set; }
+        public DelegateCommand save { get; set; }
+
+        readonly INavigationService navigation;
+        IEventAggregator events;
+
+        public EditProfilePageViewModel(INavigationService navigationService, IEventAggregator eventAgregator)
         {
+            navigation = navigationService;
+            events = eventAgregator;
+
+            cancel = new DelegateCommand(OnCancelClick);
+            save = new DelegateCommand(OnSaveClick);
+        }
+
+        public void OnCancelClick(){
+
+            navigation.GoBackAsync();
+
+        }
+
+        public void OnSaveClick(){
+
+            navigation.GoBackAsync();
         }
     }
 }
