73a384b1718826bf7511285ed874ec2ed1625acf
Miguel Oviedo <migueloviedoc@gmail.com>
2018-01-09 21:53:22 -0800

    Adds about us page

---
+++ StyleUs/View/AboutUsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace StyleUs.View
+{
+    public partial class AboutUsPage : ContentPage
+    {
+        public AboutUsPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+
+        }
+    }
+}
