b06ab567eb6d2181939948bf3953a9ca0a9a9643
Miguel Oviedo <migueloviedoc@gmail.com>
2018-01-09 21:58:34 -0800

    Adds about us page again

--- StyleUs/ViewModel/AboutUsPageViewModel.cs
+++ StyleUs/ViewModel/AboutUsPageViewModel.cs
@@ -1,10 +1,45 @@
 ﻿using System;
+using System;
+
+using System.ComponentModel;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+using StyleUs.View;
+using Prism.Navigation;
+using Prism.Commands;
+using Prism.Events;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
 namespace StyleUs.ViewModel
 {
-    public class AboutUsPageViewModel
+    public class AboutUsPageViewModel : ContentPage
     {
-        public AboutUsPageViewModel()
+
+        public DelegateCommand back { get; set; }
+
+        readonly INavigationService navigation;
+        IEventAggregator events;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public AboutUsPageViewModel(INavigationService navigationService, IEventAggregator eventAgregator)
         {
+            navigation = navigationService;
+            events = eventAgregator;
+
+            back = new DelegateCommand(OnBackClick);
+        }
+
+
+        public void OnBackClick()
+        {
+            //navigation.NavigateAsync("AboutUsPage");
+            navigation.GoBackAsync();
+            //navigation.NavigateAsync(new Uri("/MainTabbedPage/MenuPage", UriKind.Absolute));
+
         }
     }
 }
